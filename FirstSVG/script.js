127bc953db49d4765818ed9d7aa0b5de3928c8a1
basil-ahmed <90772853+basil-ahmed@users.noreply.github.com>
2022-10-02 05:23:22 +0400

    Add files via upload

---
+++ FirstSVG/script.js
@@ -0,0 +1,8 @@
+let headlight = document.getElementById("Clickable");
+
+headlight.addEventListener('click', () => {
+
+    let result = document.getElementById("result");
+    result.innerHTML = "You just clicked on the headlights!";
+
+})
